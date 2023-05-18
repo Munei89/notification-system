9f5e01fe922ec712c1012a1471b738d4ac4c24f7
Munei Nengwenani <flint.m9@gmail.com>
2023-05-18 11:22:00 +0200

    fix: update animations

--- src/components/Notification/Notification.Styles.js
+++ src/components/Notification/Notification.Styles.js
@@ -9,6 +9,41 @@ export const progressBar = (width) => keyframes`
     }
 `;
 
+export const pulsate = keyframes`
+   from {
+      -webkit-transform: scale(1);
+              transform: scale(1);
+      -webkit-transform-origin: center center;
+              transform-origin: center center;
+      -webkit-animation-timing-function: ease-out;
+              animation-timing-function: ease-out;
+    }
+    10% {
+      -webkit-transform: scale(0.91);
+              transform: scale(0.91);
+      -webkit-animation-timing-function: ease-in;
+              animation-timing-function: ease-in;
+    }
+    17% {
+      -webkit-transform: scale(0.98);
+              transform: scale(0.98);
+      -webkit-animation-timing-function: ease-out;
+              animation-timing-function: ease-out;
+    }
+    33% {
+      -webkit-transform: scale(0.87);
+              transform: scale(0.87);
+      -webkit-animation-timing-function: ease-in;
+              animation-timing-function: ease-in;
+    }
+    45% {
+      -webkit-transform: scale(1);
+              transform: scale(1);
+      -webkit-animation-timing-function: ease-out;
+              animation-timing-function: ease-out;
+    }
+`;
+
 const notificationColors = {
   success: "#49C674",
   info: "#289DF4",
@@ -63,6 +98,7 @@ export const NotificationItem = styled.div`
 
   svg {
     z-index: 1;
+    animation: ${pulsate} 1.5s ease-in-out 4 both;
   }
 `;
 
